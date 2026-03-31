648b8ebb8c4f57555f9b8f5e90b582c0445ace92
PhucAn04 <pham27282@gmail.com>
2026-03-31 21:15:21 +0700

    Initial commit and add Livewire

---
+++ src/resources/views/livewire/counter.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <button wire:click="increment" style="margin-top:20px; padding: 10px 20px; font-size: 16px; cursor: pointer; background-color: #f53003; color: white; border: none; border-radius: 5px;">
+        Count: {{ $count }}
+    </button>
+</div>
